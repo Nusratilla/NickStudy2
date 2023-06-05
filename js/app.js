9a589cba1695f7c016c663c307dbf2994d782b0f
Nusratilla <nusratilla.88@gmail.com>
2023-06-05 21:43:13 +0500

    anime

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// ----------Menu Bar---------- 
+// ---------------Menu Bar--------------- 
 
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
@@ -9,13 +9,30 @@ menuIcon.addEventListener('click', () => {
 });
 
 
-// -------Accordion FAQ -------------- 
+// ------------Accordion FAQ ------------------- 
 
-const accordion = document.querySelectorAll('.contentBx')
+const accordion = document.querySelectorAll('.contentBx');
 
 for (i = 0; i<accordion.length; i++ ){
     accordion[i].addEventListener('click', function () {
         this.classList.toggle('active')
-    })
-}
+    });
+};
 
+// ---------------Scroll Animation--------------- 
+
+const hiddenElements = document.querySelectorAll('.animation-right, .animation-left, .animation-bottom');
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle('show-animation', entry.isIntersecting)
+        });
+    
+    },
+    {
+        threshold: .15,
+    }
+);
+
+hiddenElements.forEach((el) => observer.observe(el));
